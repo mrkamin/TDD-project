c937c67dc7ebf3921be162b5546ed809a2d4d247
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-05-05 01:34:19 +0500

    Add A test for factorial Method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,5 @@
+require 'RSpec/autorun'
+
 class Solver
     def factorial(n)
       # your code here
@@ -9,6 +11,25 @@ class Solver
   
     def fizzbuzz(n)
       # your code here
+    end
+  end
+
+  RSpec.describe Solver do
+    describe '#factorial' do
+      it 'returns 1 when given 0' do
+        solver = Solver.new
+        expect(solver.factorial(0)).to eq 1
+      end
+  
+      it 'returns the factorial of a positive integer' do
+        solver = Solver.new
+        expect(solver.factorial(5)).to eq 120
+      end
+  
+      it 'raises an exception when given a negative integer' do
+        solver = Solver.new
+        expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+      end
     end
   end
   
