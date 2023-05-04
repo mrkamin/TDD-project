2d0cadef8663dab2d459c765b323aad98518dcd1
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-05-05 03:25:41 +0500

    Fix Linter Error

--- solver.rb
+++ solver.rb
@@ -1,86 +1,90 @@
+# frozen_string_literal: true
+
 require 'RSpec/autorun'
 
+# A class that provides methods to solve various problems
 class Solver
-    def factorial(n)
-        if n < 0
-          raise ArgumentError, "Argument must be a positive integer"
-        end
-    
-        result = 1
-        for i in 1..n
-          result *= i
-        end
-        result
-      end
-  
-    def reverse(word)
-     word.reverse
+  def factorial(num)
+    raise ArgumentError, 'Argument must be a positive integer' if num.negative?
+
+    result = 1
+    (1..num).each do |i|
+      result *= i
     end
-  
-    def fizzbuzz(n)
-      if n % 3 == 0 && n % 5 == 0
-      "fizzbuzz"
-    elsif n % 3 == 0
-      "fizz"
-    elsif n % 5 == 0
-      "buzz"
+    result
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    raise ArgumentError, 'Argument must be a positive integer' if num <= 0
+
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
     else
-      n.to_s
+      num.to_s
     end
-  end
-    end
-  end
-
-  RSpec.describe Solver do
-    describe '#factorial' do
-      it 'returns 1 when given 0' do
-        solver = Solver.new
-        expect(solver.factorial(0)).to eq 1
-      end
-  
-      it 'returns the factorial of a positive integer' do
-        solver = Solver.new
-        expect(solver.factorial(5)).to eq 120
-      end
-  
-      it 'raises an exception when given a negative integer' do
-        solver = Solver.new
-        expect { solver.factorial(-1) }.to raise_error(ArgumentError)
-      end
-    end
-  end
-# Add test for reverse method
-describe "#reverse" do
-    it "reverses the input string" do
-      solver = Solver.new
-      expect(solver.reverse("hello")).to eq("olleh")
-    end
-    
-    describe "#fizzbuzz" do
-  it "returns 'fizz' when the input is divisible by 3" do
-    solver = Solver.new
-    expect(solver.fizzbuzz(9)).to eq("fizz")
-  end
-
-  it "returns 'buzz' when the input is divisible by 5" do
-    solver = Solver.new
-    expect(solver.fizzbuzz(10)).to eq("buzz")
-  end
-
-  it "returns 'fizzbuzz' when the input is divisible by 3 and 5" do
-    solver = Solver.new
-    expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
-  end
-
-  it "returns the input as a string when the input is not divisible by 3 or 5" do
-    solver = Solver.new
-    expect(solver.fizzbuzz(7)).to eq("7")
-  end
-
-  it "raises an ArgumentError when the input is 0 or negative" do
-    solver = Solver.new
-    expect { solver.fizzbuzz(-1) }.to raise_error(ArgumentError)
-    expect { solver.fizzbuzz(0) }.to raise_error(ArgumentError)
   end
 end
+
+RSpec.describe Solver do
+  describe '#factorial' do
+    it 'returns 1 when given 0' do
+      solver = Solver.new
+      expect(solver.factorial(0)).to eq 1
+    end
+
+    it 'returns the factorial of a positive integer' do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq 120
+    end
+
+    it 'raises an exception when given a negative integer' do
+      solver = Solver.new
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
   end
+end
+# Add test for reverse method
+# rubocop:disable Metrics/BlockLength
+describe '#reverse' do
+  it 'reverses the input string' do
+    solver = Solver.new
+    expect(solver.reverse('hello')).to eq('olleh')
+  end
+  # rubocop:enable Metrics/BlockLength
+  # Add a test for Fizzbuzz metode
+  describe '#fizzbuzz' do
+    it "returns 'fizz' when the input is divisible by 3" do
+      solver = Solver.new
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it "returns 'buzz' when the input is divisible by 5" do
+      solver = Solver.new
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it "returns 'fizzbuzz' when the input is divisible by 3 and 5" do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns the input as a string when the input is not divisible by 3 or 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+
+    it 'raises an ArgumentError when the input is 0 or negative' do
+      solver = Solver.new
+      expect { solver.fizzbuzz(-1) }.to raise_error(ArgumentError)
+      expect { solver.fizzbuzz(0) }.to raise_error(ArgumentError)
+    end
+  end
+end
