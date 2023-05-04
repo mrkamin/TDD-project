d49624577d8273d66dc4363d92657264b6dcc255
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-05-04 22:49:52 +0100

    add a test for fuzz method

--- solver.rb
+++ solver.rb
@@ -46,5 +46,32 @@ describe "#reverse" do
       solver = Solver.new
       expect(solver.reverse("hello")).to eq("olleh")
     end
+    
+    describe "#fizzbuzz" do
+  it "returns 'fizz' when the input is divisible by 3" do
+    solver = Solver.new
+    expect(solver.fizzbuzz(9)).to eq("fizz")
   end
-  
+
+  it "returns 'buzz' when the input is divisible by 5" do
+    solver = Solver.new
+    expect(solver.fizzbuzz(10)).to eq("buzz")
+  end
+
+  it "returns 'fizzbuzz' when the input is divisible by 3 and 5" do
+    solver = Solver.new
+    expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
+  end
+
+  it "returns the input as a string when the input is not divisible by 3 or 5" do
+    solver = Solver.new
+    expect(solver.fizzbuzz(7)).to eq("7")
+  end
+
+  it "raises an ArgumentError when the input is 0 or negative" do
+    solver = Solver.new
+    expect { solver.fizzbuzz(-1) }.to raise_error(ArgumentError)
+    expect { solver.fizzbuzz(0) }.to raise_error(ArgumentError)
+  end
+end
+  end
